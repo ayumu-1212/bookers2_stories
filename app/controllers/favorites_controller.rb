c62efc211b7ca9fec5c99b102c16a64edb81e717
ayumu-1212 <ayumuabe1434@gmail.com>
2021-08-30 15:07:47 +0000

    :bookmark: いいね機能にajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
     favorite = Favorite.new(book_id: params[:book_id], user_id: current_user.id)
     favorite.save
-    redirect_back(fallback_location: book_path(params[:book_id]))
+    # redirect_back(fallback_location: book_path(params[:book_id]))
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_back(fallback_location: book_path(params[:book_id]))
+    # redirect_back(fallback_location: book_path(params[:book_id]))
   end
 end
