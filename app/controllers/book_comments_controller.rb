feb59a0348deba28a5257798a0853d1dfe061351
ayumu-1212 <ayumuabe1434@gmail.com>
2021-08-30 16:20:31 +0000

    :bookmark: コメント機能のajax実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,5 +1,7 @@
 class BookCommentsController < ApplicationController
   def create
+    @book = Book.find(params[:book_id])
+    @book_comment_new = BookComment.new
     @book_comment = BookComment.new(
       book_id: params[:book_id],
       user_id: current_user.id,
@@ -7,18 +9,16 @@ class BookCommentsController < ApplicationController
     )
     if @book_comment.save
       flash[:notice] = "コメントの投稿が完了しました"
-      redirect_to book_path(params[:book_id])
     else
-      @book = Book.find(params[:book_id])
       @user = @book.user
       render "books/show"
     end
   end
   
   def destroy
+    @book = Book.find(params[:book_id])
     book_comment = BookComment.find(params[:id])
     book_comment.destroy
-    redirect_to book_path(params[:book_id])
   end
   
   def book_comment_params
